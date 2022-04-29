50a6f27538f4fbd27f77a52642f49015a280254e
zhanqh50 <zhanqh50@chinaunicom.cn>
2022-04-29 15:38:45 +0800

    开启集群模式

--- redis-cluster/prepare.sh
+++ redis-cluster/prepare.sh
@@ -11,7 +11,4 @@ cp redis.conf /docker/redis/700{1..6}/config/redis.conf
 echo "创建容器……"
 docker-compose -f redis-cluster.yml up -d
 
-redis-cli -h 124.221.109.245 -p 7001 --cluster create  --cluster-replicas 1 \
-	124.221.109.245:7001 124.221.109.245:7002 \
-	124.221.109.245:7003 124.221.109.245:7004 \
-	124.221.109.245:7005 124.221.109.245:7006
+echo "请手动指定集群的分布，使用\n\tdocker exec -it redis1 reddis-cli --cluster create --cluster-replicas 0 host:ip"
